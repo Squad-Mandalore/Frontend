524c86f28eb9f0b73da93c8d6b5174ccec97370e
BB223 <79644542+BB223@users.noreply.github.com>
2024-02-26 18:12:22 +0100

    chore: SM-87 refactor to use property binding

--- src/app/password-box/password-box.component.ts
+++ src/app/password-box/password-box.component.ts
@@ -18,25 +18,28 @@ import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 })
 export class PasswordBoxComponent implements ControlValueAccessor {
 
-    disabled = false;
+    disabled: boolean = false;
     value!: string;
-    onChange = (value: string) => { };
-    onTouched = () => { };
-    touched = false;
+    onChange: (value: string) => void = (_: string) => { };
+    onTouched: () => void = () => { };
+    touched: boolean = false;
+    width: string = '20';
+    backgroundColor: string = '#ff6347';
+    textContent: string = 'Schwach';
 
     constructor() {
 
     }
 
-    registerOnChange(onChange: any) {
+    registerOnChange(onChange: (_: string) => void): void {
         this.onChange = onChange;
     }
 
-    registerOnTouched(onTouched: any) {
+    registerOnTouched(onTouched: () => void): void {
         this.onTouched = onTouched;
     }
 
-    setDisabledState(isDisabled: boolean) {
+    setDisabledState(isDisabled: boolean): void {
         this.disabled = isDisabled;
     }
 
@@ -46,33 +49,26 @@ export class PasswordBoxComponent implements ControlValueAccessor {
 
     onInput(value: string): void {
         this.value = value;
-
-        const passwordStrengthFill = document.getElementById('password-strength-fill');
-        const passwordStrengthText = document.getElementById('password-strength-text');
         const passwordLength = this.value.length;
 
         // Evaluate password strength
-        let strength = '';
         if (passwordLength < 5) {
-            strength = 'Schwach';
-            (<HTMLDivElement>passwordStrengthFill).style.width = '20%'; // Red
-            (<HTMLDivElement>passwordStrengthFill).style.backgroundColor = '#ff6347';
+            this.textContent = 'Schwach';
+            this.width = '20%'; // Red
+            this.backgroundColor = '#ff6347';
         } else if (passwordLength < 10) {
-            strength = 'Mittel';
-            (<HTMLDivElement>passwordStrengthFill).style.width = '50%'; // Orange
-            (<HTMLDivElement>passwordStrengthFill).style.backgroundColor = '#ffa500';
+            this.textContent = 'Mittel';
+            this.width = '50%'; // Orange
+            this.backgroundColor = '#ffa500';
         } else if (passwordLength >= 10 && passwordLength < 12) {
-            strength = 'Gut';
-            (<HTMLDivElement>passwordStrengthFill).style.width = '70%'; // Green
-            (<HTMLDivElement>passwordStrengthFill).style.backgroundColor = '#2ecc71';
+            this.textContent = 'Gut';
+            this.width = '70%'; // Green
+            this.backgroundColor = '#2ecc71';
         } else {
-            strength = 'Sehr stark';
-            (<HTMLDivElement>passwordStrengthFill).style.width = '100%'; // Green
-            (<HTMLDivElement>passwordStrengthFill).style.backgroundColor = '#2ecc71';
+            this.textContent = 'Sehr stark';
+            this.width = '100%'; // Green
+            this.backgroundColor = '#2ecc71';
         }
-
-        // Display password strength
-        (<HTMLDivElement>passwordStrengthText).textContent = 'Passwortstärke: ' + strength;
 
         this.onChange(this.value);
     }
