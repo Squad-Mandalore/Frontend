43496974bd8b8a11778328f40a96ee10d95cded9
Ronny Vater <ronny.vater@telekom.de>
2024-02-27 17:07:18 +0100

    Added logic for secondary-button component

--- src/app/components/buttons/secondary-button/secondary-button.component.ts
+++ src/app/components/buttons/secondary-button/secondary-button.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IconComponent } from '../../icon/icon.component';
 
 @Component({
   selector: 'app-secondary-button',
   standalone: true,
-  imports: [],
-  templateUrl: './secondary-button.component.html',
-  styleUrl: './secondary-button.component.scss'
+  imports: [CommonModule, IconComponent],
+  template: `
+    <button class="secondary-button" [ngClass]="{ 'with-icon': showIcon }">
+      <span>{{ text }}</span>
+      <ng-container *ngIf="showIcon">
+        <app-icon [iconName]="iconName" [iconColor]="iconColor"></app-icon>
+      </ng-container>
+      <ng-content></ng-content>
+    </button>
+  `,
+  styleUrls: ['./secondary-button.component.scss'],
 })
-export class SecondaryButtonComponent {
 
+
+export class SecondaryButtonComponent {
+  @Input() showIcon: Boolean = false;
+  @Input() text: string = '';
+  @Input() iconName: string = '';
+  @Input() iconColor: string = "var(--accent-900)";
 }
