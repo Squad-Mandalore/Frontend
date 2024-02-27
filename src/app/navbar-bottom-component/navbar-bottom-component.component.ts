cc2a58dba46786432e635354701ef299660bd987
Marvin <hofmann.marvin03@gmail.com>
2024-02-27 15:57:50 +0100

    added function for determining active state of navigation icon

--- src/app/navbar-bottom-component/navbar-bottom-component.component.ts
+++ src/app/navbar-bottom-component/navbar-bottom-component.component.ts
@@ -1,16 +1,27 @@
 import { Component } from '@angular/core';
 import { UserCardComponent } from '../user-card/user-card.component';
 import { CommonModule } from '@angular/common';
+import { RouterModule, ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-navbar-bottom-component',
   standalone: true,
-  imports: [UserCardComponent, CommonModule],
+  imports: [UserCardComponent, CommonModule, RouterModule],
   templateUrl: './navbar-bottom-component.component.html',
   styleUrl: './navbar-bottom-component.component.scss'
 })
 export class NavbarBottomComponentComponent {
-  isActive: boolean = true; // logik für active state hier
+  urlParts: any = [];
+
+  constructor(private route: ActivatedRoute) {
+    this.urlParts = this.route.snapshot.url.map(segment => segment.toString());
+  }
+
+  checkIfIsActive(routeParameter : string){
+    return this.urlParts.includes(routeParameter);
+  }
+
   user = {
     id: '1',
     firstname: 'Kay',
