9201f1c55fb4b4c532ec895c9df9e67b3b59ccbe
Marvin <hofmann.marvin03@gmail.com>
2024-03-10 15:03:49 +0100

    refac: element substitution through components

--- src/app/create-trainer-modal/create-trainer-modal.component.ts
+++ src/app/create-trainer-modal/create-trainer-modal.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { PrimaryButtonComponent } from '../components/buttons/primary-button/primary-button.component';
 import { SecondaryButtonComponent } from '../components/buttons/secondary-button/secondary-button.component';
+import { IconComponent } from '../components/icon/icon.component';
 
 @Component({
   selector: 'app-create-trainer-modal',
   standalone: true,
-  imports: [PrimaryButtonComponent, SecondaryButtonComponent],
+  imports: [PrimaryButtonComponent, SecondaryButtonComponent, IconComponent, PrimaryButtonComponent],
   templateUrl: './create-trainer-modal.component.html',
   styleUrl: './create-trainer-modal.component.scss'
 })
