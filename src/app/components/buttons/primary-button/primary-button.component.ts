125ef80498b6774b72682b2d16f251f4faa7a28c
Ronny Vater <ronny.vater@telekom.de>
2024-03-11 12:03:21 +0100

    feat: added tooltip hover-effect to quaternary button

--- src/app/components/buttons/primary-button/primary-button.component.ts
+++ src/app/components/buttons/primary-button/primary-button.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IconComponent } from '../../icon/icon.component';
-import { DeclarationListEmitMode } from '@angular/compiler';
 
 @Component({
   selector: 'app-primary-button',
