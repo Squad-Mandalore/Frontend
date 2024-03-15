103ef0afcb83dcb31f1026b386522b6211cd5c3c
Markus Siegert <spongemarkus@gmail.com>
2024-03-15 10:37:00 +0100

    chore: SM-193 commenting, typing

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -1,16 +1,17 @@
-import { HttpClient, HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { HttpInterceptorFn } from '@angular/common/http';
 import { LocalStorageService } from '../services/local-storage.service';
 import { inject } from '@angular/core';
-import { catchError, switchMap, throwError } from 'rxjs';
+import { EMPTY, catchError, switchMap, throwError } from 'rxjs';
 import { AuthService, Token } from '../shared/generated';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
+    // First case: login request
     if (req.url.includes('auth/login')) {
         return next(req);
     }
-    let localStorageService = inject(LocalStorageService);
+    // Second case: refresh token request
+    let localStorageService: LocalStorageService = inject(LocalStorageService);
     if (req.url.includes('auth/refresh')) {
-        console.log('refresh')
         return next(req).pipe(
             catchError((error) => {
                 if (error.status != 401) {
@@ -19,23 +20,27 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
 
                 //TODO logout
                 localStorageService.clear();
+                return EMPTY;
             })
         );
     }
+    // Third case: any other request
     let newreq = req;
     const accessToken = localStorageService.getItem('access_token');
     if (accessToken) {
         newreq = req.clone({ headers: req.headers.set('Authorization', `Bearer ${accessToken}`) });
     }
-    let authService = inject(AuthService);
+    let authService: AuthService = inject(AuthService);
     return next(newreq).pipe(
+        // If the accessToken is invalid, we try to refresh it
         catchError((error) => {
             if (error.status != 401) {
                 return throwError(() => error);
             }
-            
+
+            // Triggers second case and refreshes the token then retries the request
             return authService.refreshAccessTokenAuthRefreshPost(localStorageService.getItem('refresh_token')).pipe(
-                switchMap((token) => {
+                switchMap((token: Token) => {
                     localStorageService.setItem('access_token', token.access_token);
                     localStorageService.setItem('refresh_token', token.refresh_token);
 
