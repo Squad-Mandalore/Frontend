af23c6b605034f6864dfc94e40fbe2925079d2a5
BB223 <79644542+BB223@users.noreply.github.com>
2024-03-17 13:58:15 +0100

    chore: write test

--- src/app/shared/auth-extention.service.spec.ts
+++ src/app/shared/auth-extention.service.spec.ts
@@ -1,16 +1,34 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthExtentionService } from './auth-extention.service';
+import { LocalStorageService } from './local-storage.service';
 
 describe('AuthExtentionService', () => {
-  let service: AuthExtentionService;
+    let service: AuthExtentionService;
+    let localStorageService: LocalStorageService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(AuthExtentionService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(AuthExtentionService);
+        localStorageService = TestBed.inject(LocalStorageService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+
+    it('should clear localStorge', () => {
+        service.logout();
+        expect(localStorageService.getItem('access_token')).toBeNull();
+    });
+
+    it('should be logged in', () => {
+        localStorageService.setItem('access_token', 'test_token');
+        expect(service.isLoggedIn()).toBeTrue();
+    });
+
+    it('should not be logged in', () => {
+        service.logout();
+        expect(service.isLoggedIn()).toBeFalse();
+    });
 });
