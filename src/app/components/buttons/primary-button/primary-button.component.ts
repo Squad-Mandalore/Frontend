03aa92e3a2d61c43c2bd362a250044b665176d81
BB223 <79644542+BB223@users.noreply.github.com>
2024-03-17 20:42:53 +0100

    chore: add disabled property to buttons

--- src/app/components/buttons/primary-button/primary-button.component.ts
+++ src/app/components/buttons/primary-button/primary-button.component.ts
@@ -3,40 +3,41 @@ import { CommonModule } from '@angular/common';
 import { IconComponent } from '../../icon/icon.component';
 
 @Component({
-  selector: 'app-primary-button',
-  standalone: true,
-  imports: [CommonModule, IconComponent],
-  templateUrl: './primary-button.component.html',
-  styleUrls: ['./primary-button.component.scss'],
+    selector: 'app-primary-button',
+    standalone: true,
+    imports: [CommonModule, IconComponent],
+    templateUrl: './primary-button.component.html',
+    styleUrls: ['./primary-button.component.scss'],
 })
 
 export class PrimaryButtonComponent {
-  // @Input() showIcon: Boolean = false;
-  @Input() text: string = '';
-  @Input() iconName: string = '';
-  @Input() iconColor: string = "var(--accent-0)";
-  @Input() showLoader: boolean = false;
-  @Input() description: string = '';
-  @Input() minWidth: string = '80px';
-  @Input() strokeWidth: string = '2.3';
-  @Input() iconWidth: string = '15';
-  @Input() iconHeight: string = '15';
-  tempIcon : string = '';
+    // @Input() showIcon: Boolean = false;
+    @Input() text: string = '';
+    @Input() iconName: string = '';
+    @Input() iconColor: string = "var(--accent-0)";
+    @Input() showLoader: boolean = false;
+    @Input() description: string = '';
+    @Input() minWidth: string = '80px';
+    @Input() strokeWidth: string = '2.3';
+    @Input() iconWidth: string = '15';
+    @Input() iconHeight: string = '15';
+    @Input() disabled: boolean = false;
+    tempIcon: string = '';
 
-  activateLoader(){
-    if(this.showLoader === true && this.iconName !== ''){
-      this.tempIcon = this.iconName;
-      this.iconName = "loader";
+    activateLoader() {
+        if (this.showLoader === true && this.iconName !== '') {
+            this.tempIcon = this.iconName;
+            this.iconName = "loader";
 
-      setTimeout(()=>{
-        this.deactivateLoader()
-      }, 2000)
+            setTimeout(() => {
+                this.deactivateLoader()
+            }, 2000)
+        }
     }
-  }
 
-  deactivateLoader(){
-    if(this.iconName === 'loader'){
-      this.iconName = this.tempIcon;
+    deactivateLoader() {
+        if (this.iconName === 'loader') {
+            this.iconName = this.tempIcon;
+        }
     }
-  }
 }
