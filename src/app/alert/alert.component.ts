7bcd82d3497cfd58560b706013fe8b629a0e62ae
BB223 <79644542+BB223@users.noreply.github.com>
2024-03-17 13:33:49 +0100

    ci: SM-116 login and failed login capabilities

--- src/app/alert/alert.component.ts
+++ src/app/alert/alert.component.ts
@@ -1,15 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from "@angular/common"
 
 @Component({
-  selector: 'app-alert',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './alert.component.html',
-  styleUrl: './alert.component.scss'
+    selector: 'app-alert',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './alert.component.html',
+    styleUrl: './alert.component.scss'
 })
 export class AlertComponent {
-  @Input() alertTitle!: string;
-  @Input() alertDescription!: string;
-  @Input() alertType?: string;
+    @Input() alertTitle?: string;
+    @Input() alertDescription?: string;
+    @Input() alertType?: 'success' | 'error';
+
+    @Output() closed = new EventEmitter<void>();
+
+    onClick() {
+        this.closed.emit();
+    }
 }
--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,13 +1,18 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import { LocalStorageService } from '../services/local-storage.service';
 import { AthletesService, AuthService } from '../shared/generated';
+import { Router } from '@angular/router';
+import { AlertComponent } from '../alert/alert.component';
+import { LoggerService } from '../shared/logger.service';
+import { LocalStorageService } from '../shared/local-storage.service';
+import { AuthExtentionService } from '../shared/auth-extention.service';
+import { NgIf } from '@angular/common';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
     selector: 'app-login-page',
     standalone: true,
-    imports: [ReactiveFormsModule],
+    imports: [ReactiveFormsModule, AlertComponent, NgIf],
     templateUrl: './login-page.component.html',
     styleUrl: './login-page.component.scss'
 })
@@ -16,18 +21,40 @@ export class LoginPageComponent {
         username: ['', Validators.required],
         password: ['', Validators.required],
     });
+    alertTitle?: string;
+    alertDescription?: string;
+    isError: boolean = false;
+
     constructor(
         private formBuilder: FormBuilder,
-        private localStorageService: LocalStorageService,
         private authService: AuthService,
+        private authExtService: AuthExtentionService,
         private athleteService: AthletesService,
-    ) { }
+        private router: Router,
+        private logger: LoggerService,
+        private localStorageService: LocalStorageService,
+    ) {
+        if (this.authExtService.isLoggedIn()) {
+            this.router.navigate(['/athleten']);
+        }
+    }
 
     onSubmit() {
-        this.authService.loginForAccessTokenAuthLoginPost(this.loginForm.value.username!, this.loginForm.value.password!).subscribe(token => {
-            this.localStorageService.setItem('access_token', token.access_token);
-            this.localStorageService.setItem('refresh_token', token.refresh_token);
-            //TODO redirect to Dashboard
+        this.authService.loginForAccessTokenAuthLoginPost(this.loginForm.value.username!, this.loginForm.value.password!).subscribe({
+            next: (token) => {
+                this.localStorageService.setItem('access_token', token.access_token);
+                this.localStorageService.setItem('refresh_token', token.refresh_token);
+                this.router.navigate(['/athleten']);
+            },
+            error: (error: HttpErrorResponse) => {
+                this.alertTitle = 'Login fehlgeschlagen';
+                this.alertDescription = 'Benutzername oder Passwort falsch!';
+                this.isError = true;
+            }
         });
+    }
+
+    alertClosed() {
+        this.isError = false;
     }
 }
