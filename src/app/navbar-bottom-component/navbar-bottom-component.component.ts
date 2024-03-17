1aa3ed55597c99071a4f369529fa57842406b379
BB223 <79644542+BB223@users.noreply.github.com>
2024-03-17 13:36:09 +0100

    chore: fixed website for bigger screens

--- src/app/navbar-bottom-component/navbar-bottom-component.component.ts
+++ src/app/navbar-bottom-component/navbar-bottom-component.component.ts
@@ -2,31 +2,35 @@ import { Component } from '@angular/core';
 import { UserCardComponent } from '../user-card/user-card.component';
 import { CommonModule } from '@angular/common';
 import { RouterModule, ActivatedRoute } from '@angular/router';
-
+import { AuthExtentionService } from '../shared/auth-extention.service';
 
 @Component({
-  selector: 'app-navbar-bottom-component',
-  standalone: true,
-  imports: [UserCardComponent, CommonModule, RouterModule],
-  templateUrl: './navbar-bottom-component.component.html',
-  styleUrl: './navbar-bottom-component.component.scss'
+    selector: 'app-navbar-bottom-component',
+    standalone: true,
+    imports: [UserCardComponent, CommonModule, RouterModule],
+    templateUrl: './navbar-bottom-component.component.html',
+    styleUrl: './navbar-bottom-component.component.scss'
 })
 export class NavbarBottomComponentComponent {
-  urlParts: any = [];
+    urlParts: string[] = [];
 
-  constructor(private route: ActivatedRoute) {
-    this.urlParts = this.route.snapshot.url.map(segment => segment.toString());
-  }
+    constructor(private route: ActivatedRoute, private authExtService: AuthExtentionService) {
+        this.urlParts = this.route.snapshot.url.map(segment => segment.toString());
+    }
 
-  checkIfIsActive(routeParameter : string){
-    return this.urlParts.includes(routeParameter);
-  }
+    checkIfIsActive(routeParameter: string) {
+        return this.urlParts.includes(routeParameter);
+    }
 
-  user = {
-    id: '1',
-    firstname: 'Kay',
-    lastname: 'Schulz',
-    type: 'Administrator',
-    username: 'KaySchulz42'
-  }
+    onClick() {
+        this.authExtService.logout();
+    }
+
+    user = {
+        id: '1',
+        firstname: 'Kay',
+        lastname: 'Schulz',
+        type: 'Administrator',
+        username: 'KaySchulz42'
+    }
 }
