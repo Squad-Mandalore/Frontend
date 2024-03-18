36603aa61d04179fbb5385d1157ff86c1dad094e
BB223 <79644542+BB223@users.noreply.github.com>
2024-03-18 22:24:51 +0100

    chore: login

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,30 +1,29 @@
 import { Component } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import { AthletesService, AuthService } from '../shared/generated';
+import { AthletesService, AuthService, Token } from '../shared/generated';
 import { Router } from '@angular/router';
 import { AlertComponent } from '../alert/alert.component';
 import { LoggerService } from '../shared/logger.service';
 import { LocalStorageService } from '../shared/local-storage.service';
 import { AuthExtentionService } from '../shared/auth-extention.service';
-import { NgIf } from '@angular/common';
+import { NgClass } from '@angular/common';
 import { HttpErrorResponse } from '@angular/common/http';
 import { PrimaryButtonComponent } from '../components/buttons/primary-button/primary-button.component';
 
 @Component({
     selector: 'app-login-page',
     standalone: true,
-    imports: [ReactiveFormsModule, AlertComponent, NgIf, PrimaryButtonComponent],
+    imports: [ReactiveFormsModule, AlertComponent, NgClass, PrimaryButtonComponent],
     templateUrl: './login-page.component.html',
     styleUrl: './login-page.component.scss'
 })
 export class LoginPageComponent {
-    loginForm = this.formBuilder.group({
-        username: ['', Validators.required],
-        password: ['', Validators.required],
-    });
+    loginForm;
     alertTitle?: string;
     alertDescription?: string;
-    isError: boolean = false;
+    isError: boolean;
+    closeAlert;
+    timeout!: ReturnType<typeof setTimeout>;
 
     constructor(
         private formBuilder: FormBuilder,
@@ -38,24 +37,38 @@ export class LoginPageComponent {
         if (this.authExtService.isLoggedIn()) {
             this.router.navigate(['/athleten']);
         }
+        this.loginForm = this.formBuilder.group({
+            username: ['', Validators.required],
+            password: ['', Validators.required],
+        });
+        this.isError = false;
+        this.closeAlert = () => {
+            clearTimeout(this.timeout);
+            this.isError = false;
+        }
     }
 
     onSubmit() {
         this.authService.loginForAccessTokenAuthLoginPost(this.loginForm.value.username!, this.loginForm.value.password!).subscribe({
-            next: (token) => {
+            next: (token: Token) => {
                 this.localStorageService.setItem('access_token', token.access_token);
                 this.localStorageService.setItem('refresh_token', token.refresh_token);
                 this.router.navigate(['/athleten']);
             },
             error: (error: HttpErrorResponse) => {
-                this.alertTitle = 'Login fehlgeschlagen';
-                this.alertDescription = 'Benutzername oder Passwort falsch!';
-                this.isError = true;
+                this.displayAlert();
             }
         });
     }
 
-    alertClosed() {
-        this.isError = false;
+    displayAlert() {
+        if (this.isError) {
+            return;
+        }
+        this.alertTitle = 'Login fehlgeschlagen';
+        this.alertDescription = 'Benutzername oder Passwort falsch!';
+        this.isError = true;
+        this.timeout = setTimeout(this.closeAlert, 4000);
     }
 }
+
