3a8c8fcc4338f845a55e8d4ac96869169e5c58ce
Lucas Knesel <Lucas04K@lk.mr.kuh@gmail.com>
2024-03-19 07:30:06 +0100

    Fix standalone declaration for PasswordBoxComponent

--- src/app/password-box/password-box.component.spec.ts
+++ src/app/password-box/password-box.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PasswordBoxComponent } from './password-box.component';
 
 describe('PasswordBoxComponent', () => {
@@ -8,10 +7,10 @@ describe('PasswordBoxComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PasswordBoxComponent]
+      declarations: [PasswordBoxComponent]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(PasswordBoxComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -20,4 +19,52 @@ describe('PasswordBoxComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should update password strength to "Weak" for a password with less than 5 characters', () => {
+    component.onInput('1234'); // Less than 5 characters
+    expect(component.strengthTextContent).toEqual('Weak');
+    expect(component.strengthBarWidth).toEqual('20%');
+    expect(component.strengthBarColor).toEqual('#ff6347');
+  });
+
+  it('should update password strength to "Medium" for a password with 5 to 9 characters', () => {
+    component.onInput('abcd123'); // 7 characters
+    expect(component.strengthTextContent).toEqual('Medium');
+    expect(component.strengthBarWidth).toEqual('50%');
+    expect(component.strengthBarColor).toEqual('#ff6347');
+  });
+
+  it('should update password strength to "Medium" for a password with 10 to 11 characters', () => {
+    component.onInput('strongPwd12'); // 10 characters
+    expect(component.strengthTextContent).toEqual('Medium');
+    expect(component.strengthBarWidth).toEqual('75%');
+    expect(component.strengthBarColor).toEqual('#ff6347');
+  });
+
+  it('should update password strength to "Strong" for a password with 12 or more characters', () => {
+    component.onInput('VeryStrongPwd!'); // 13 characters
+    expect(component.strengthTextContent).toEqual('Strong');
+    expect(component.strengthBarWidth).toEqual('100%');
+    expect(component.strengthBarColor).toEqual('#2ecc71');
+  });
+
+  it('should call onChange method when input value changes', () => {
+    const onChangeSpy = spyOn(component, 'onChange');
+    component.onInput('test123');
+    expect(onChangeSpy).toHaveBeenCalledWith('test123');
+  });
+
+  it('should set disabled state', () => {
+    component.setDisabledState(true);
+    expect(component.disabled).toEqual(true);
+    component.setDisabledState(false);
+    expect(component.disabled).toEqual(false);
+  });
+
+  it('should write value to input field', () => {
+    const newValue = 'newPassword123';
+    component.writeValue(newValue);
+    expect(component.value).toEqual(newValue);
+  });
+
 });
--- src/app/password-box/password-box.component.ts
+++ src/app/password-box/password-box.component.ts
@@ -1,73 +1,122 @@
-import { Component } from '@angular/core';
+import { Component, forwardRef } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
+/**
+ * This component provides a password input box with password strength indicator.
+ */
 @Component({
     selector: 'app-password-box',
-    standalone: true,
-    imports: [],
     templateUrl: './password-box.component.html',
-    styleUrl: './password-box.component.scss',
+    styleUrls: ['./password-box.component.scss'],
     providers: [
         {
             provide: NG_VALUE_ACCESSOR,
-            multi: true,
-            useExisting: PasswordBoxComponent
+            useExisting: forwardRef(() => PasswordBoxComponent),
+            multi: true
         }
-    ]
-
+    ],
 })
 export class PasswordBoxComponent implements ControlValueAccessor {
 
+    /**
+     * Whether the input is disabled or not.
+     */
     disabled: boolean = false;
+
+    /**
+     * The value of the input.
+     */
     value!: string;
+
+    /**
+     * Callback function to be called when the value changes.
+     */
     onChange: (value: string) => void = (_: string) => { };
+
+    /**
+     * Callback function to be called when the input is touched.
+     */
     onTouched: () => void = () => { };
+
+    /**
+     * Indicate if the input has been touched.
+     */
     touched: boolean = false;
-    width: string = '20';
-    backgroundColor: string = '#ff6347';
-    textContent: string = 'Schwach';
 
-    constructor() {
+    /**
+     * The width of the password strength bar.
+     */
+    strengthBarWidth: string = '20%';
 
-    }
+    /**
+     * The background color of the password strength bar.
+     */
+    strengthBarColor: string = '#ff6347';
 
+    /**
+     * The text content of the password strength.
+     */
+    strengthTextContent: string = 'Weak';
+
+    constructor() { }
+
+    /**
+     * Register callback function to be called when the value changes.
+     * @param onChange Callback function
+     */
     registerOnChange(onChange: (_: string) => void): void {
         this.onChange = onChange;
     }
 
+    /**
+     * Register callback function to be called when the input is touched.
+     * @param onTouched Callback function
+     */
     registerOnTouched(onTouched: () => void): void {
         this.onTouched = onTouched;
     }
 
+    /**
+     * Set the disabled state of the input.
+     * @param isDisabled Whether the input should be disabled or not
+     */
     setDisabledState(isDisabled: boolean): void {
         this.disabled = isDisabled;
     }
 
+    /**
+     * Write value to the input.
+     * @param value Value to be written to the input
+     */
     writeValue(value: string): void {
         this.value = value;
     }
 
+    /**
+     * Handle input event, update password strength and call onChange.
+     * @param value Input value
+     */
     onInput(value: string): void {
         this.value = value;
         const passwordLength = this.value.length;
 
         // Evaluate password strength
         if (passwordLength < 5) {
-            this.textContent = 'Schwach';
-            this.width = '20%'; // Red
-            this.backgroundColor = '#ff6347';
+            this.strengthTextContent = 'Weak';
+            this.strengthBarWidth = '20%'; // Red
+            this.strengthBarColor = '#ff6347'; // Red
         } else if (passwordLength < 10) {
-            this.textContent = 'Mittel';
-            this.width = '50%'; // Orange
-            this.backgroundColor = '#ffa500';
+            this.strengthTextContent = 'Medium';
+            this.strengthBarWidth = '50%'; // Red
+            this.strengthBarColor = '#ff6347'; // Red
         } else if (passwordLength >= 10 && passwordLength < 12) {
-            this.textContent = 'Gut';
-            this.width = '70%'; // Green
-            this.backgroundColor = '#2ecc71';
+            this.strengthTextContent = 'Medium';
+            this.strengthBarWidth = '75%'; // Red
+            this.strengthBarColor = '#ff6347'; // Red
         } else {
-            this.textContent = 'Sehr stark';
-            this.width = '100%'; // Green
-            this.backgroundColor = '#2ecc71';
+            this.strengthTextContent = 'Strong';
+            this.strengthBarWidth = '100%'; // Green
+            this.strengthBarColor = '#2ecc71'; // Green
         }
 
         this.onChange(this.value);
