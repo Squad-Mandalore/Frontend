50342160662c1ff2b3fc81e38a20129abd486cf9
Ronny Vater <ronny.vater@telekom.de>
2024-03-25 11:16:04 +0100

    init branch

---
+++ src/app/discipline-menu/discipline-menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-discipline-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './discipline-menu.component.html',
+  styleUrl: './discipline-menu.component.scss'
+})
+export class DisciplineMenuComponent {
+
+}
