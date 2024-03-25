e3a42679bb5f6f29f09d262e67cfc27e20f03550
BB223 <79644542+BB223@users.noreply.github.com>
2024-03-25 10:43:37 +0100

    ci: SM-33 add input Form

--- src/app/create-trainer-modal/create-trainer-modal.component.ts
+++ src/app/create-trainer-modal/create-trainer-modal.component.ts
@@ -1,16 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { PrimaryButtonComponent } from '../components/buttons/primary-button/primary-button.component';
 import { SecondaryButtonComponent } from '../components/buttons/secondary-button/secondary-button.component';
 import { IconComponent } from '../components/icon/icon.component';
+import { PasswordBoxComponent } from '../components/password-box/password-box.component';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AlertComponent } from '../alert/alert.component';
+import { AlertService } from '../shared/alert.service';
 
 @Component({
   selector: 'app-create-trainer-modal',
   standalone: true,
-  imports: [PrimaryButtonComponent, SecondaryButtonComponent, IconComponent, PrimaryButtonComponent],
+  imports: [PrimaryButtonComponent, SecondaryButtonComponent, IconComponent, PasswordBoxComponent, ReactiveFormsModule, AlertComponent],
   templateUrl: './create-trainer-modal.component.html',
   styleUrl: './create-trainer-modal.component.scss'
 })
 export class CreateTrainerModalComponent {
-  passwordStrength = "schwach";
-  progressBarColor = "var(--danger)";
+  @Output() click = new EventEmitter<any>();
+
+  onButtonClick() {
+    console.log("close");
+    this.click.emit();
+  }
+  
+  trainerForm;
+  constructor(private formBuilder: FormBuilder, private alertService: AlertService){
+    this.trainerForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+  });
+  }
+
+  onSubmit(){
+    console.log('createTrainer');
+    if(false){
+      this.click.emit();
+    }else{
+      this.alertService.show('Erstellung fehlgeschlagen','Bei der Erstellung ist etwas schief gelaufen! Bitte nochmal versuchen.',"error");
+    }
+  }
 }
