41e3cfc71e04d3e7b624a804702feabfd69bf763
BB223 <79644542+BB223@users.noreply.github.com>
2024-04-04 13:44:33 +0200

    fix: added deleted redirect routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,9 @@ import {LoginPageComponent} from "./pages/login-page/login-page.component";
 import {authGuard} from "./shared/guard/auth.guard";
 
 export const routes: Routes = [
+    {path: '', redirectTo: 'athleten', pathMatch: 'full'},
     {path: "login", component: LoginPageComponent},
     {path: "athleten", component: DashboardPageComponent, canActivate: [authGuard]},
     {path: "athleten/:id", component: DashboardPageComponent},
-    {path: '', redirectTo: 'athleten', pathMatch: 'full'}
+    {path: "**", redirectTo: 'athleten'}
 ];
