d9539bb59ce1f9ced182053597f13f1a5177c430
Marvin <hofmann.marvin03@gmail.com>
2024-04-04 21:57:53 +0200

    ci: implemented delete function for exercise - fails because of backend error

--- src/app/pages/exercise-overview-page/exercise-overview-page.component.ts
+++ src/app/pages/exercise-overview-page/exercise-overview-page.component.ts
@@ -12,6 +12,7 @@ import customSort from '../../../utils/custom-sort';
 import { ConfirmationModalComponent } from '../../components/confirmation-modal/confirmation-modal.component';
 import { AlertService } from '../../shared/alert.service';
 import { HttpErrorResponse } from '@angular/common/http';
+import { ConfirmationService } from '../../shared/confirmation.service';
 
 @Component({
   selector: 'app-exercise-overview',
@@ -22,7 +23,7 @@ import { HttpErrorResponse } from '@angular/common/http';
 })
 
 export class ExerciseOverviewComponent implements OnInit, OnDestroy {
-  constructor(private alertService: AlertService, private exerciseService: ExercisesService) { }
+  constructor(private alertService: AlertService, private exerciseService: ExercisesService, private confirmationService: ConfirmationService) { }
   exercises: ExerciseResponseSchema[] = []
   filter: any = {};
   sorting: {property: string, direction: "asc" | "desc"} = {property: 'category', direction: 'asc'};
@@ -35,14 +36,21 @@ export class ExerciseOverviewComponent implements OnInit, OnDestroy {
     },
     createAthleteModal: {
       isActive: false,
-    },
-    confirmationModal: {
-      isActive: false,
-      modalTitle: "Benutzer wirklich löschen?",
-      modalDescription: "Mit dieser Aktion wird der ausgewählte Benutzer unwiderruflich gelöscht.",
-      primaryButtonText: "Benutzer löschen",
-      secondaryButtonText: "Abbrechen",
     }
+  }
+
+  deleteExercise(exercise: ExerciseResponseSchema){
+    this.confirmationService.show("Möchtest du die Übung wirklich löschen?", "Dieser Vorgang kann nicht rückgängig gemacht werden.", "Löschen", "Abbrechen", true, ()=>{
+      this.exerciseService.deleteAhtleteExercisesIdDelete(exercise.id).subscribe({
+        next: ()=>{
+          this.exercises.filter(element => element.id !== exercise.id);
+          this.alertService.show("Löschen erfolgreich", "Die Übung wurde erfolgreich gelöscht", "success");
+        },
+        error: (error: HttpErrorResponse) => {
+          this.alertService.show('Löschen der Übung fehlgeschlagen', 'Bitte versuche es später erneut', "error");
+        }
+      })
+    })
   }
 
   setSorting(property: string){
