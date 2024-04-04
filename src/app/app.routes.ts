e69384ebe0bbcbd847028c3ba8583b7cd35eeb4c
BB223 <79644542+BB223@users.noreply.github.com>
2024-04-04 13:47:06 +0200

    Merge SM-244 Visualisiere automatischen Logout to dev (#11)
    * ci: SM-244 add alert on forced logout
    
    * fix: SM-244 fix
    
    * fix: added deleted redirect routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,9 @@ import {LoginPageComponent} from "./pages/login-page/login-page.component";
 import {authGuard} from "./shared/guard/auth.guard";
 
 export const routes: Routes = [
+    {path: '', redirectTo: 'athleten', pathMatch: 'full'},
     {path: "login", component: LoginPageComponent},
     {path: "athleten", component: DashboardPageComponent, canActivate: [authGuard]},
-    {path: "athleten/:id", component: DashboardPageComponent}
+    {path: "athleten/:id", component: DashboardPageComponent},
+    {path: "**", redirectTo: 'athleten'}
 ];
