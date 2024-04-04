92d4ef1bb224d9887b2418f41697e7252bd41cec
Marvin <hofmann.marvin03@gmail.com>
2024-04-04 11:15:53 +0200

    refac: switched close icon in confirmation modal to app-icon component & moved clicks for confirm/cancel directly to confirmationService

--- src/app/components/confirmation-modal/confirmation-modal.component.ts
+++ src/app/components/confirmation-modal/confirmation-modal.component.ts
@@ -3,25 +3,15 @@ import { PrimaryButtonComponent } from '../buttons/primary-button/primary-button
 import { SecondaryButtonComponent } from '../buttons/secondary-button/secondary-button.component';
 import { ConfirmationService } from '../../shared/confirmation.service';
 import { NgIf } from '@angular/common';
+import { IconComponent } from '../icon/icon.component';
 
 @Component({
   selector: 'app-confirmation-modal',
   standalone: true,
-  imports: [PrimaryButtonComponent, SecondaryButtonComponent, NgIf],
+  imports: [PrimaryButtonComponent, SecondaryButtonComponent, NgIf, IconComponent],
   templateUrl: './confirmation-modal.component.html',
   styleUrl: './confirmation-modal.component.scss'
 })
 export class ConfirmationModalComponent {
-  @Input() iconName: string = "";
-  @Input() isDeleteButton: boolean = false;
-
-  confirmEvent(){
-    this.confirmationService.Confirm();
-  }
-
-  cancelEvent(){
-    this.confirmationService.Cancel();
-  }
-
   constructor(protected confirmationService: ConfirmationService){}
 }
