5c2bd14b056670ede46887bc60399faea3211d96
BB223 <79644542+BB223@users.noreply.github.com>
2024-05-22 19:44:27 +0200

    Nginxconf (#23)
    * minor bug fixes, nicer result formatting
    
    * Sm 278 (#22)
    
    * ci: SM-278 better completes component
    
    * chore: move for orga
    
    * ci: SM-278 add patch files
    
    * ci: SM-278: finishing patch completes
    
    * fix: SM-99999 typo bro
    
    * fix: SM-278 ignor invalid input
    
    ---------
    
    Co-authored-by: Markus Siegert <markus.siegert@telekom.de>
    
    * fix: oppsie while merging
    
    * some
    
    * fix: changes in responsibility
    
    * merged responsivity fix and color changing
    
    * ci: SM-999 add nginx conf for reverse proxy
    
    * fix: exercise selection fixed
    
    * fix: amend
    
    * ci: reverse_proxy
    
    * fix: everything
    
    ---------
    
    Co-authored-by: MrLappes <79363858+MrLappes@users.noreply.github.com>
    Co-authored-by: Markus Siegert <markus.siegert@telekom.de>
    Co-authored-by: Marvin <hofmann.marvin03@gmail.com>

---
+++ src/app/components/completes-modal/patch-completes-modal/patch-completes-modal.component.ts
@@ -0,0 +1,92 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { CompletesResponseSchema, CompletesService } from '../../../shared/generated';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AlertService } from '../../../shared/alert.service';
+import { PrimaryButtonComponent } from '../../buttons/primary-button/primary-button.component';
+import { SecondaryButtonComponent } from '../../buttons/secondary-button/secondary-button.component';
+import { CommonModule } from '@angular/common';
+import { DistanceinputComponent } from '../distanceinput/distanceinput.component';
+import { TimeinputComponent } from '../timeinput/timeinput.component';
+import { IconComponent } from '../../icon/icon.component';
+
+@Component({
+  selector: 'app-patch-completes',
+  standalone: true,
+  imports: [PrimaryButtonComponent, SecondaryButtonComponent, CommonModule, DistanceinputComponent, TimeinputComponent, IconComponent, ReactiveFormsModule],
+  templateUrl: './patch-completes-modal.component.html',
+  styleUrl: './patch-completes-modal.component.scss'
+})
+export class PatchCompletesComponent implements OnInit {
+  completesForm;
+  subPage = 1;
+  @Input({ required: true }) selectedCompletes!: CompletesResponseSchema;
+  @Input({ required: true }) modal: any;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private alertService: AlertService,
+    private completesService: CompletesService,
+  ) {
+    this.completesForm = this.formBuilder.group({
+      result: ['', Validators.required],
+      quantity: [''],
+    })
+  }
+  ngOnInit(): void {
+    console.log(this.selectedCompletes);
+    this.switchSubPage(this.selectedCompletes.result);
+  }
+
+  switchSubPage(givenValue: string) {
+    if (givenValue.match(/^\d{4}$/)) {
+      this.completesForm.patchValue({ quantity: givenValue });
+      this.subPage = 1;
+    } else if (givenValue.match(/^\d{2}:\d{2}:\d{2}:\d{3}$/)) {
+      this.subPage = 2;
+    } else if (givenValue.match(/^\d{3}:\d{3}:\d{2}$/)) {
+      this.subPage = 3;
+    } else {
+      this.subPage = 4;
+    }
+    this.completesForm.patchValue({ result: givenValue });
+  }
+
+  onSubmit() {
+    if (this.completesForm.value.quantity != '') {
+      const quantity = +this.completesForm.value.quantity!;
+      this.completesForm.patchValue({ result: this.submitNewQuantity(quantity) });
+    }
+
+    if(!this.completesForm.valid) {
+      return;
+    }
+
+    this.completesService.updateCompletesCompletesPatch(this.selectedCompletes.exercise.id, this.selectedCompletes.athlete_id, this.selectedCompletes.tracked_at, { result: this.completesForm.value.result! }).subscribe({
+      next: (response: CompletesResponseSchema) => {
+        this.alertService.show('Eintrag erfasst', 'Eintrag wurde erfolgreich hinzugefügt.', 'success');
+        this.modal.isActive = false;
+        this.selectedCompletes.result = response.result;
+        this.selectedCompletes.points = response.points;
+      },
+      error: (error) => {
+        this.alertService.show('Erfassung fehlgeschlagen', 'Bei der Erfassung ist etwas schief gelaufen', "error");
+      }
+    })
+  }
+
+  submitNewQuantity(quantity: number): string {
+    let quantityResult = '';
+
+    if (quantity < 10) {
+      quantityResult = '000' + quantity.toString();
+    } else if (quantity < 100) {
+      quantityResult = '00' + quantity.toString();
+    } else if (quantity < 1000) {
+      quantityResult = '0' + quantity.toString();
+    } else {
+      quantityResult = quantity.toString();
+    }
+
+    return quantityResult;
+  }
+}
---
+++ src/app/components/completes-modal/timeinput/timeinput.component.ts
@@ -0,0 +1,123 @@
+import { Component } from '@angular/core';
+import { AbstractControl, ControlValueAccessor, NG_VALIDATORS, NG_VALUE_ACCESSOR, ValidationErrors, Validator } from '@angular/forms';
+
+@Component({
+  selector: 'app-timeinput',
+  standalone: true,
+  imports: [],
+  templateUrl: './timeinput.component.html',
+  styleUrl: './timeinput.component.scss',
+  providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            multi: true,
+            useExisting: TimeinputComponent
+        },
+        {
+            provide: NG_VALIDATORS,
+            multi: true,
+            useExisting: TimeinputComponent
+        }
+      ]
+})
+export class TimeinputComponent implements ControlValueAccessor , Validator{
+  validate(control: AbstractControl<any, any>): ValidationErrors | null {
+    if (!control.value.match(/^\d{2}:\d{2}:\d{2}:\d{3}$/)) {
+      return {
+        illegalInput: true
+      }
+    }
+    return null;
+  }
+  registerOnValidatorChange?(fn: () => void): void {
+    this.onValChange = fn;
+  }
+  hours: string = '';
+  minutes: string = '';
+  seconds: string = '';
+  milliseconds: string = '';
+  // Whether the input is disabled or not.
+  disabled: boolean = false;
+
+  // The value of the input.
+  value: string = "";
+
+  // Callback function to be called when the value changes.
+  onValChange: () => void = () => { };
+
+  // Callback function to be called when the value changes.
+  onChange: (value: string) => void = (_: string) => { };
+
+  // Callback function to be called when the input is touched.
+  onTouched: () => void = () => { };
+
+  // Indicate if the input has been touched.
+  touched: boolean = false;
+
+  writeValue(value: string): void {
+    this.value = value;
+    const time: string[] = this.value.split(':');
+    this.hours = time[0];
+    this.minutes = time[1];
+    this.seconds = time[2];
+    this.milliseconds = time[3];
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  change(hours: number, minutes: number, seconds: number, milliseconds:number){
+    let combinedSeconds = ((hours || 0) * 3600) + ((minutes || 0) * 60) + ((seconds || 0) * 1) + ((milliseconds || 0) * 0.001);
+
+    let hoursResult = '';
+    let minutesResult = '';
+    let secondsResult = '';
+    let millisecondsResult = '';
+
+    // Stunden
+    let hoursForOutput = Math.floor(combinedSeconds / 3600);
+    if(hoursForOutput < 10) {
+      hoursResult = '0'+hoursForOutput.toString();
+    } else {
+      hoursResult = hoursForOutput.toString();
+    }
+    combinedSeconds %= 3600;
+
+    // Minuten
+    let minutesForOutput = Math.floor(combinedSeconds / 60);
+    if(minutesForOutput < 10) {
+      minutesResult = '0'+minutesForOutput.toString();
+    } else {
+      minutesResult = minutesForOutput.toString();
+    }
+    combinedSeconds %= 60;
+
+    // Sekunden
+    let secondsForOutput = Math.floor(combinedSeconds / 1);
+    if(secondsForOutput < 10) {
+      secondsResult = '0'+secondsForOutput.toString();
+    } else {
+      secondsResult = secondsForOutput.toString();
+    }
+    combinedSeconds %= 1;
+
+    // Millisekunden
+    let millisecondsForOutput = Math.floor(combinedSeconds * 1000);
+    if(millisecondsForOutput < 10) {
+      millisecondsResult = '00'+millisecondsForOutput.toString();
+    } else if(millisecondsForOutput > 9 && millisecondsForOutput < 100) {
+      millisecondsResult = '0'+millisecondsForOutput.toString();
+    } else {
+      millisecondsResult = millisecondsForOutput.toString();
+    }
+
+    this.value = hoursResult+':'+minutesResult+':'+secondsResult+':'+millisecondsResult;
+    this.onChange(this.value);
+  }
+}
